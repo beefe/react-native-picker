ad0e3ba7b35526688e88522dcca56a7c07d969ef
Michał Chudziak <michal.chudziak@gmail.com>
2016-11-21 21:25:26 +0100

    Cleanup JS.

--- index.js
+++ index.js
@@ -1,83 +1,84 @@
 import {
-    Platform,
-    NativeModules,
-    NativeAppEventEmitter
+  Platform,
+  NativeModules,
+  NativeAppEventEmitter,
 } from 'react-native';
 
-let ios = Platform.OS === 'ios';
-let android = Platform.OS === 'android';
-let Picker = NativeModules.BEEPickerManager;
+const isIos = Platform.OS === 'ios';
+const isAndroid = Platform.OS === 'android';
+const Picker = NativeModules.BEEPickerManager;
 
-export default {
+const NOOP = () => {};
 
-    init(options){
-        let opt = {
-            isLoop: false,
-            pickerConfirmBtnText: '确认',
-            pickerCancelBtnText: '取消',
-            pickerTitleText: '请选择',
-            pickerBg: [196, 199, 206, 1],
-            pickerToolBarBg: [232, 232, 232, 1],
-            pickerTitleColor: [20, 20, 20, 1],
-            pickerCancelBtnColor: [1, 186, 245, 1],
-            pickerConfirmBtnColor: [1, 186, 245, 1],
-            onPickerConfirm(){},
-            onPickerCancel(){},
-            onPickerSelect(){},
-            ...options
-        };
-        let fnConf = {
-            confirm: opt.onPickerConfirm,
-            cancel: opt.onPickerCancel,
-            select: opt.onPickerSelect
-        };
+class ReactNativePicker {
+  listener = null;
 
-        Picker._init(opt);
-        //there are no `removeListener` for NativeAppEventEmitter & DeviceEventEmitter
-        this.listener && this.listener.remove();
-        this.listener = NativeAppEventEmitter.addListener('pickerEvent', event => {
-            if(ios){
-                fnConf[event['type']](event['selectedValue']);
-            }
-            else if(android){
-                for (let i in event){
-                    typeof fnConf[i] === 'function' && fnConf[i](event[i]);
-                }
-            }
-        });
-    },
+  init(options) {
+    const fullOptions = {
+      isLoop: false,
+      pickerConfirmBtnText: '确认',
+      pickerCancelBtnText: '取消',
+      pickerTitleText: '请选择',
+      pickerBg: [196, 199, 206, 1],
+      pickerToolBarBg: [232, 232, 232, 1],
+      pickerTitleColor: [20, 20, 20, 1],
+      pickerCancelBtnColor: [1, 186, 245, 1],
+      pickerConfirmBtnColor: [1, 186, 245, 1],
+      onPickerConfirm: NOOP,
+      onPickerCancel: NOOP,
+      onPickerSelect: NOOP,
+      ...options,
+    };
 
-    show(){
-        Picker.show();
-    },
+    const pickerFunctions = {
+      confirm: fullOptions.onPickerConfirm,
+      cancel: fullOptions.onPickerCancel,
+      select: fullOptions.onPickerSelect,
+    };
 
-    hide(){
-        Picker.hide();
-    },
+    Picker._init(fullOptions);
 
-    toggle(){
-        this.isPickerShow(show => {
-            if(show){
-                this.hide();
-            }
-            else{
-                this.show();
-            }
-        });
-    },
+    this.listener && this.listener.remove();
+    this.listener = NativeAppEventEmitter.addListener('pickerEvent', (events) => {
+        if (isIos) {
+          pickerFunctions[events['type']](events['selectedValue']);
+        }
+        else if (android) {
+          for (let key in events) {
+            typeof pickerFunctions[key] === 'function' && pickerFunctions[key](events[key]);
+          }
+        }
+    });
+  }
 
-    isPickerShow(fn){
-        //android return two params: err(error massage) and status(show or not)
-        //ios return only one param: hide or not...
-        Picker.isPickerShow((err, status) => {
-            let returnValue = null;
-            if(android){
-                returnValue = err ? false : status;
-            }
-            else if(ios){
-                returnValue = !err;
-            }
-            fn(returnValue);
-        });
-    }
+  show() {
+    Picker.show();
+  }
+
+  hide() {
+    Picker.hide();
+  }
+
+  toggle() {
+    this.isPickerShow((show) => {
+      show ? this.hide() : this.show();
+    });
+  }
+
+  isPickerShow(callback) {
+    /* Android returns two params - err and status, iOS returns only status. */
+    Picker.isPickerShow((err, status) => {
+      let returnValue = null;
+
+      if (isIos) {
+        returnValue = !err;
+      } else if (isAndroid) {
+        returnValue = err ? false : status;
+      }
+
+      callback(returnValue);
+    });
+  }
 };
+
+export default new ReactNativePicker();
