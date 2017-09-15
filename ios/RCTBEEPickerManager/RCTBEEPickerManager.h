365f355515100fb7769b33670934361b3767a32b
Mark Rickert <mjar81@gmail.com>
2017-09-15 13:05:20 -0600

    Fixes impot of RCTBridgeModule.h for newer versions or React Native

--- ios/RCTBEEPickerManager/RCTBEEPickerManager.h
+++ ios/RCTBEEPickerManager/RCTBEEPickerManager.h
@@ -7,7 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RCTBridgeModule.h"
+
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
 
 @interface RCTBEEPickerManager : NSObject<RCTBridgeModule>
 
