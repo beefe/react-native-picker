e8ff6cc7c2b960e46fa0bba096448a1e0b3e8063
xwenliang <wenliang.web@gmail.com>
2017-12-07 18:29:23 +0800

    unlink picker

--- example/PickerTest/android/app/src/main/java/com/pickertest/MainApplication.java
+++ example/PickerTest/android/app/src/main/java/com/pickertest/MainApplication.java
@@ -3,7 +3,6 @@ package com.pickertest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.beefe.picker.PickerViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new PickerViewPackage()
+          new MainReactPackage()
       );
     }
 
